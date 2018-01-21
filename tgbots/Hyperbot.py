0a5bd2676d767aaeec8332f0e5749b1c9dee77e8
VisaJE <eemil@live.com>
2018-01-21 22:05:22 +0200

    Workign on a file

---
+++ tgbots/Hyperbot.py
@@ -0,0 +1,34 @@
+#Raaka pohja
+
+import telepot
+import mysql
+import time
+import getpass
+
+#TODO Connecting to server 
+def connect():
+	pass
+
+def getHighscore():
+	pass
+
+def broadcaster
+_username = input('Enter username for SQL: ')
+_password = getpass.getpass()
+
+try:
+    connect
+except Exception as e:
+    print(e)
+
+#Sets the bot up with a required token.
+with open('/opt/secrets/Hypersense_token.txt', 'r') as t:
+	bot = telepot.Bot(t)
+	bot.message_loop(handler)
+
+
+	
+	
+while 1:
+	time.sleep(60)
+	
