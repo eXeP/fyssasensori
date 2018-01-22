b51edb915af3e3d2ab8aac438dce31636ff5858a
VisaJE <eemil@live.com>
2018-01-22 23:31:17 +0200

    Working on telepot stuff.

--- tgbots/Hyperbot.py
+++ tgbots/Hyperbot.py
@@ -9,7 +9,8 @@ import pgdb
 #TODO Connecting to server 
 
 	pass
-
+started = false
+stored_id = 'unknown'
 def getHighscore():
 	pass
 
@@ -21,6 +22,26 @@ hostname = 'localhost'
 db = ''
 connection = pgdb.connect(host = hostname, user = _username, password = _password, database = db)
 
+def handler(msg):
+    try:
+        text = msg['text']
+        chat_id = msg['chat']['id']
+        if started:
+            if text = '/heiluttelu':
+                #TODO
+        else
+            if text = '/(^^)':
+            with open('./stored_id.txt, w+) as f:
+                started = true
+                if stored_id = f.read() == '':
+                    f.write(str(chat_id ))
+                    stored_id = chat_id
+                bot.sendMessage(stored_id,
+                                'Jassoo.',
+                                reply_to_message_id=msg['message_id']
+                                )
+    except: 
+        pass
 
 #Sets the bot up with a required token.
 with open('/opt/secrets/Hypersense_token.txt', 'r') as t:
@@ -31,5 +52,5 @@ with open('/opt/secrets/Hypersense_token.txt', 'r') as t:
 	
 	
 while 1:
-	time.sleep(60)
+	time.sleep(30)
 	
