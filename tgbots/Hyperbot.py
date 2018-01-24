4260ebeec64b1e2cd7f60daedef0f1a41cffce98
VisaJE <eemil.visakorpi@gmail.com>
2018-01-24 22:13:17 +0200

    Functional bot version.

--- tgbots/Hyperbot.py
+++ tgbots/Hyperbot.py
@@ -6,8 +6,8 @@ import pgdb
 from datetime import timedelta
 
 # Get config
-with open('/opt/secrets/token.txt', 'r') as config:
-    content = config.read().replace(" ", "").split(':')
+with open('/opt/secrets/liikemittari_config.txt', 'r') as config:
+    content = config.read().replace(" ", "").replace("\n","=").split('=')
     token = content[content.index('token') + 1]
     username = content[content.index('sqluser') + 1]
     password = content[content.index('sqlpassword') + 1]
@@ -20,18 +20,19 @@ with open('/opt/secrets/token.txt', 'r') as config:
 # Set up necessary variables and connect
 started = False
 stored_id = 'unknown'
+stored_msg = 'ukno'
 highscore = 0
 connection = pgdb.connect(host=hostname,
                           user=username,
                           password=password,
                           database=dbase
                           )
-cur = connection.cursor
+cur = connection.cursor()
 
 
 def get_highscore():
     cur.execute("SELECT * FROM %s ORDER \
-                BY %s DESC, 'date' LIMIT 1" % (table, mname))
+                BY %s DESC, date LIMIT 1" % (table, mname))
     return cur.fetchone()
 
 
@@ -43,42 +44,44 @@ def get_user(input_name):
 
 # @param conditional checks whether the found highscore
 # is higher than the known one.
-def print_hs_data(conditional):
+def print_hs_data(conditional, t_index):
+    global highscore
     result = get_highscore()
     if conditional:
         if highscore >= result.amount:
             return ''
     score = result.amount
+    highscore = score
     time = result.date + timedelta(hours=2)
-    text = 'Kovin heiluttelija on %s indeksin arvolla %s.\n \
-            Saavutus  tehtiin %s' % (result.name, score, time)
+    if t_index == 1:
+        text = 'Kovin heiluttelija on %s indeksin arvolla %s.\n \
+Saavutus  tehtiin %s.' % (result.name, score, time)
+    else:
+        text = 'Uskomatonta! %s heilutteli \
+arvon %s %s.' % (result.name, score, time)
     return text
 
 
 def handler(msg):
+    global started
+    global stored_id
+    global stored_msg
     try:
         text = msg['text']
         chat_id = msg['chat']['id']
-        if started:
-            if text.lower().find('heiluttelija') + 1:
-                send_highscore(msg['message_id'], print_hs_data(False))
+        if text.lower().find('heilu') + 1:
+            send_highscore(chat_id, print_hs_data(False, 1))
         else:
             if text == '/(^^)':
                 with open('./stored_id.txt', 'w+') as memory:
-                    global started
                     started = True
-                    text = memory.read()
-                    if text == '':
-                        memory.write(str(chat_id))
-                        stored_id = chat_id
-                    else:
-                        stored_id = text
+                    text = memory.read()    
+                    stored_id = chat_id
                     bot.sendMessage(stored_id,
                                     'Moro!',
                                     reply_to_message_id=msg['message_id']
                                     )
             elif text == '/Stahp':
-                global started
                 started = False
                 bot.sendMessage(stored_id,
                                 "Ookoo.",
@@ -88,11 +91,10 @@ def handler(msg):
         pass
 
 
-def send_highscore(msgid, text):
+def send_highscore(ch_id, text):
     if text != '':
-        bot.sendMessage(stored_id,
-                        text,
-                        reply_to_message_id=stored_id
+        bot.sendMessage(ch_id,
+                        text
                         )
 
 
@@ -104,4 +106,4 @@ bot.message_loop(handler)
 while 1:
     time.sleep(10)
     if started:
-        send_highscore(stored_id, print_hs_data(True))
+        send_highscore(stored_id, print_hs_data(True, 2))
