3f291ab3935042bd367613af596ebaa16e7b023f
VisaJE <eemil@live.com>
2018-10-25 18:22:57 +0300

    Movesense files are now pushed to the raw folder

--- sensor/build.sh
+++ sensor/build.sh
@@ -7,6 +7,6 @@ ninja dfupkg
 cp *.zip ../
 cd ..
 rm -r buildtempfolder2
-mv Movesense_dfu.zip movesense_dfu.zip
-mv Movesense_dfu_w_bootloader.zip movesense_dfu_w_bootloader.zip
+mv Movesense_dfu.zip ../FYSSASENSORI-ANDROID/src/main/res/raw/movesense_dfu.zip
+mv Movesense_dfu_w_bootloader.zip ../FYSSASENSORI-ANDROID/src/main/res/raw/movesense_dfu_w_bootloader.zip
 
